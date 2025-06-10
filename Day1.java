91871089b7beb7a3ce4a1dfd46ef60983c0b7dec
YashJagtap1005 <yashujagtap444@gmail.com>
2025-06-10 12:51:31 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,60 @@
+public class Day1 {
+
+    public static void main(String [] arg)
+    {
+    //     System.out.println("hello world");
+
+    //     int a = 10;
+    //     char first = 'a';
+    //     boolean second = false;
+    //     byte b = 25;
+    //     long w = 50429120L;
+    //     float pi = 3.14f;
+    //     String s = "yash";
+        
+    //     System.out.println(second);
+    //     System.out.println(w);
+    //     System.out.println(pi);
+    //     System.out.println(a);
+    //     System.out.println(b);
+    //     System.out.println(first);
+
+
+        // int a = 100;
+        // int b = 200;
+        // char first = 'a';
+        // char second = 'b';
+
+        // String n1 = "10";
+        // String n2 = "15";
+
+        
+        // int a1 = Integer.parseInt(n1);
+        // int b1 = Integer.parseInt(n2);
+        
+        // String n4 = n1+a;
+        // String n5 = a+n1;
+
+        // System.out.println(50-n1);
+
+        // System.out.println(first + second);
+        
+        // System.out.println("a + b = "+(a+b));
+        // System.out.println("a - b = "+(a-b));
+        // System.out.println("a * b = "+(a*b));
+        // System.out.println("a / b = "+(a/b));
+
+        // System.out.println(n4);
+        // System.out.println(n5);
+
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a<b);
+        System.out.println(a>b);
+        // System.out.println(0 || 1);
+
+    }
+}
+
